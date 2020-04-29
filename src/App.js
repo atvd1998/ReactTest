402f2e44561c31070cd9d591aa018eaf237a2c2b
atvd <iuhthanhtung4498@gmail.com>
2020-04-29 14:47:24 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+const axios = require('axios');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gaTau: [],
+      chuyenDi: [],
+    };
+    this.getChuyenDi = this.getChuyenDi.bind(this);
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://k.vnticketonline.vn/api/GTGV/LoadDmGa'
+      );
+      console.log(response.data);
+      await this.setState((prevState) => ({
+        gaTau: [...prevState.gaTau, ...response.data],
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getChuyenDi(gaDi, gaDen) {
+    try {
+      const response = await axios.get(
+        'https://k.vnticketonline.vn/api/GTGV/LoadDmTau?maGaDen=' +
+          gaDen +
+          '&maGaDi=' +
+          gaDi +
+          '&ngayDi=2020-04-29'
+      );
+
+      await this.setState((prevState) => ({
+        chuyenDi: [...prevState.chuyenDi, ...response.data],
+      }));
+      console.log(response.data);
+      console.log(this.state.chuyenDi);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.state.gaTau.map((gaden) => {
+              this.getChuyenDi('DNA', gaden.MaGa);
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Lấy dữ liệu
+        </button>
+        <ul>
+          {this.state.gaTau.map((ga) => {
+            return <li key={ga.Id}>{ga.TenGa}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
